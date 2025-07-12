135c47e016700df7ae9d4061f3800303b325ec71
wroblewskigk <wroblewskigk@proton.me>
2025-07-12 11:14:59 +0200

    Implemented Z03 from 04_Custom_Datasets

--- 04_Custom_Datasets/Z03.py
+++ 04_Custom_Datasets/Z03.py
@@ -1,4 +1,34 @@
 """
 Exercise 3: Recreate model_0 we built in section 7 of
-https://www.learnpytorch.io/04_pytorch_custom_datasets/.
+https://www.learnpytorch.io/04_pytorch_custom_datasets/
 """
+
+from torch import nn
+
+class TinyVGG(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(hidden_units * 16 * 16, output_shape)  # For 64x64 input and 2x max pooling
+        )
+
+    def forward(self, x):
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.classifier(x)
+        return x
